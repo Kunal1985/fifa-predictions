24d319d1506bd416c09fe394a1de71c0b975a613
Swapnil <swapnil.k@ignitiv.com>
2018-02-23 15:23:44 +0530

    Styling changes

--- app/components/EditProfile.js
+++ app/components/EditProfile.js
@@ -30,7 +30,19 @@ class EditProfile extends Authentication {
               return (
                 <div className="row">
                     <div className="col-lg-12 col-md-12 col-sm-12">
-                        <form onSubmit={submitForm}>      
+                        <form onSubmit={submitForm}>
+                          <div className="row">
+                            <div className="col-lg-4 col-md-4 col-sm-12">
+                              <div className="form-group">
+                                  <label>Winery Name</label>
+                                  <Text field='editProfileWineryName' placeholder='Winery Name' className="form-control"/>
+                              </div>
+                            </div>
+                            <div className="col-lg-4 col-md-4 col-sm-12">
+                            </div>
+                            <div className="col-lg-4 col-md-4 col-sm-12">
+                            </div>
+                          </div>
                         </form>
                     </div>
                 </div>
